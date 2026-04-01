5e677d56eacea94e5c1098e7889d261781e564ca
acdd233 <89513811+ACDD233@users.noreply.github.com>
2026-04-02 07:05:36 +1100

    Lombok has been temporarily removed due to its instability. Unnecessary files have been excluded.

--- src/main/java/acdd/test/firsttest/entity/SystemKey.java
+++ src/main/java/acdd/test/firsttest/entity/SystemKey.java
@@ -2,9 +2,7 @@ package acdd.test.firsttest.entity;
 
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
-import lombok.Data;
 
-@Data
 @TableName("system_keys")
 public class SystemKey {
     @TableId
@@ -15,4 +13,60 @@ public class SystemKey {
     private byte[] pkEggAlpha;
     private byte[] mskBeta;
     private byte[] mskAlpha;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getParams() {
+        return params;
+    }
+
+    public void setParams(String params) {
+        this.params = params;
+    }
+
+    public byte[] getG() {
+        return g;
+    }
+
+    public void setG(byte[] g) {
+        this.g = g;
+    }
+
+    public byte[] getPkH() {
+        return pkH;
+    }
+
+    public void setPkH(byte[] pkH) {
+        this.pkH = pkH;
+    }
+
+    public byte[] getPkEggAlpha() {
+        return pkEggAlpha;
+    }
+
+    public void setPkEggAlpha(byte[] pkEggAlpha) {
+        this.pkEggAlpha = pkEggAlpha;
+    }
+
+    public byte[] getMskBeta() {
+        return mskBeta;
+    }
+
+    public void setMskBeta(byte[] mskBeta) {
+        this.mskBeta = mskBeta;
+    }
+
+    public byte[] getMskAlpha() {
+        return mskAlpha;
+    }
+
+    public void setMskAlpha(byte[] mskAlpha) {
+        this.mskAlpha = mskAlpha;
+    }
 }
